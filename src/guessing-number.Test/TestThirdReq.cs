7162f1924f683a1972d3e402a8b9c0a245005a40
Sergio Francisco <spinheiro.lf@gmail.com>
2022-10-17 18:57:18 -0300

    Modificando o assert do teste 3

--- src/guessing-number.Test/TestThirdReq.cs
+++ src/guessing-number.Test/TestThirdReq.cs
@@ -24,7 +24,6 @@ public class TestThirdReq
         }
         guessNumber.randomValue = mockValue;
         guessNumber.AnalyzePlay();
-        var actual = stringWriter.ToString().Trim();
-        actual.Should().Be("ACERTOU!");
-    }
+        stringWriter.ToString().Should().Contain("ACERTOU!");
+}
 }
